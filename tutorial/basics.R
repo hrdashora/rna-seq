0322e953b911a8bdc4a1991c9ae6127f3297e471
Himanshu Dashora <hrdashora@gmail.com>
2020-10-09 13:38:47 -0400

    Organizing directory

--- tutorial/basics.R
+++
@@ -1,7 +0,0 @@
-# Read the data into R
-small_counts <- read.table("data/small_counts.txt", header = TRUE)
-ResultsTable_small <- read.table("data/ResultsTable_small.txt", header=TRUE)
-# Sum the counts for each sample
-sample_sums = apply(small_counts, MARGIN = 2, sum)
-
-
