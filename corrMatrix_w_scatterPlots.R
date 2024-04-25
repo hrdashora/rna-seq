17cabdf1adc32b2267091d78256f20efccc05af7
Himanshu Dashora <hrdashora@gmail.com>
2024-04-25 11:32:25 -0400

    Remove unrelated scripts from repo

--- corrMatrix_w_scatterPlots.R
+++
@@ -1,4 +0,0 @@
-## This script generates a correlation matrix of gene expression between
-## SWI/SNF/ISWI components and stem cell markers. Data is sourced from from
-## in-house RNA-sequencing and publically available TCGA data.
-
